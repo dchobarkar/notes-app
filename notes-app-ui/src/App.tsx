0c301b76d45427ac1355a78347c09d59aea78532
Darshan Chobarkar <dchobarkar@gmail.com>
2024-04-22 16:57:01 +0530

    add css

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -8,12 +8,15 @@ const App = () => {
         <textarea placeholder="Content" rows={10} required />
         <button type="submit">Add Note</button>
       </form>
+
       <div className="notes-grid">
         <div className="note-item">
           <div className="notes-header">
             <button>X</button>
           </div>
+
           <h2>Note Title</h2>
+
           <p>Note Content</p>
         </div>
       </div>
