03254a2dc90116ad3da10840eeab6beb2e51420d
Darshan Chobarkar <dchobarkar@gmail.com>
2024-04-22 16:40:02 +0530

    add ui elements

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>X</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
