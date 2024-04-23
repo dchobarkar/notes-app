140682608211ffef901f7c2410286af4995993b7
Darshan Chobarkar <dchobarkar@gmail.com>
2024-04-23 17:34:47 +0530

    add note endpoint

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -14,6 +14,21 @@ app.get("/api/notes", async (req, res) => {
   res.json(notes);
 });
 
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content)
+    return res.status(400).send("title and content fields required");
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json();
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
 app.listen(5000, () => {
   console.log("server running on localhost:5000");
 });
