176b0e584c7987ecee57fe4137bee0cfcbe1c2ba
Darshan Chobarkar <dchobarkar@gmail.com>
2024-04-23 18:31:24 +0530

    populate ui with notes from our db

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
@@ -9,32 +9,26 @@ type Note = {
 };
 
 const App = () => {
-  const [notes, setNotes] = useState<Note[]>([
-    {
-      id: 1,
-      title: "note title 1",
-      content: "content 1",
-    },
-    {
-      id: 2,
-      title: "note title 2",
-      content: "content 2",
-    },
-    {
-      id: 3,
-      title: "note title 3",
-      content: "content 3",
-    },
-    {
-      id: 4,
-      title: "note title 4",
-      content: "content 4",
-    },
-  ]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/notes");
+
+        const notes: Note[] = await response.json();
+
+        setNotes(notes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNotes();
+  }, []);
 
   const handleNoteClick = (note: Note) => {
     setSelectedNote(note);
